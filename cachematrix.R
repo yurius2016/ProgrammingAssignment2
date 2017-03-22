5bbcbd7c3c7c25e591beec864e720bdd1940924f
yuriy savchuk <savchuk@math.fau.de>
2017-03-22 17:27:33 +0100

    Quiz matrix inverse first version

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
 ## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
+## The function creates a list of four functions set, get, setinv, getinv. 
+## The invererse of the matrix is stored in the variable inv.
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inverse) inv <<- inverse
+        getinv <- function() inv
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## The function returns the cached value of the inverse, if it has already been calculated
+## Otherwise it calculates it
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inver <- x$getinv()
+        if(!is.null(inver)) {
+                message("getting cached data")
+                return(inver)
+        }
+        data <- x$get()
+        inver <- solve(data, ...)
+        x$setinv(inver)
+        inver
 }
